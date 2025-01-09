68d2b37d0bd9c62a51b27342229a19705e5acd86
Dilip Kumar Madda <Dilip-Kumar.Madda@csi-online.de>
2025-01-09 15:39:22 +0100

    Added Mult and Div

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from src.math_operations import add, sub
+from src.math_operations import add, sub, mult, div
 
 
 def test_add():
@@ -10,3 +10,16 @@ def test_sub():
     assert sub(1,-1)    == 2
     assert sub(10,4)    == 6
     assert sub(5,6)     == -1
+
+def test_mult():
+    assert mult(1,2)    == 2
+    assert mult(2,2)    == 4
+    assert mult(10,20)  == 200
+    assert mult(100,13) == 1300
+
+
+def test_div():
+    assert div(1,2)    == 0.5
+    assert div(2,2)    == 1
+    assert div(10,20)  == 0.5
+    assert div(100,10) == 10
